ca14a338fe1de4b35f33a4ec497acb1122df94a2
Rodrigo Santos <rssantos07@gmail.com>
2024-06-06 14:49:24 -0300

    inserçao codigo logging em settings

--- twitter/settings.py
+++ twitter/settings.py
@@ -35,6 +35,20 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost', '127.0.0.1", "rssantos07.pythonanywhere.com"]
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "WARNING",
+    },
+}
+
 # Application definition
 
 INSTALLED_APPS = [
