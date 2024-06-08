53f19378d340287a3e84f94266b876da5bdbd024
Rodrigo Santos <rssantos07@gmail.com>
2024-06-08 15:31:42 -0300

    alteraçao aspas

--- usuario/views.py
+++ usuario/views.py
@@ -69,12 +69,11 @@ def getRoutes(request):
 @permission_classes([IsAuthenticated])
 def testEndPoint(request):
     if request.method == 'GET':
-        data = f"Congratulation {
-            request.user}, your API just responded to GET request"
+        data = f'Congratulation {request.user}, your API just responded to GET request'
         return Response({'response': data}, status=status.HTTP_200_OK)
     elif request.method == 'POST':
         text = request.POST.get('text')
-        data = f"Congratulation your API just responded to POST request with text: {text}"
+        data = f'Congratulation your API just responded to POST request with text: {text}'
         return Response({'response': data}, status=status.HTTP_200_OK)
     return Response({}, status.HTTP_400_BAD_REQUEST)
 
