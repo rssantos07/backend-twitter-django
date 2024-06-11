76e591eae7a80c8211a759b9d8e596eeef58cdd0
Rodrigo Santos <rssantos07@gmail.com>
2024-06-11 19:38:56 -0300

    correcao media_url retirado o valor

--- twitter/settings.py
+++ twitter/settings.py
@@ -129,7 +129,6 @@ USE_TZ = True
 
 
 STATIC_URL = "static/"
-MEDIA_URL = '/x/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
