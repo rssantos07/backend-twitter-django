35d73761be084689a8f1660adb4465666def1f04
Rodrigo Santos <rssantos07@gmail.com>
2024-06-11 17:52:14 -0300

    teste

--- twitter/settings.py
+++ twitter/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STATIC_URL = "static/"
 MEDIA_URL = '/media/'
 
